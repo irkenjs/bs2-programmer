43f9b664f6025d595bffa6d90923327f9d5afc7c
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-03-05 20:05:39 -0700

    revealing constructor and use send as promise

--- index.js
+++ index.js
@@ -4,103 +4,70 @@ var send = require('./lib/sendData');
 var revisions = require('./lib/revisions');
 
 var when = require('when');
-var bindCallback = require('when/node').bindCallback;
 var nodefn = require('when/node');
 
 function challenge(stream, options, cb){
-  //if doesnt have a challenge just return
   if(!options.hasOwnProperty('challenge')){
     return cb();
   }
   
   function unspool(index) {
-    return [index, index+1];
+    return [index, index + 1];
   }
 
   function predicate(index) {
-    return index >= options.challenge.length ;
+    return index >= options.challenge.length;
   }
 
   function handler(index) {
-
-    var d = when.defer();
-
-    send(stream, 1000, options.challenge.slice(index, index + 1), function(err, response){
-      if(err){ return d.reject(err); }
-
-      if(response !== options.response[index]){
-        return d.reject(new Error('Incorrect Response: ', response));
-      }
-
-      return d.resolve();
-    });
-
-    return d.promise;
+    return send(stream, 1000, options.challenge.slice(index, index + 1))
+      .then(function(response){
+        if(response !== options.response[index]){
+           throw new Error('Incorrect Response: ', response);
+        }
+      });
   }
 
-  //sigh hes deprecating this too
-  //https://github.com/cujojs/when/issues/370
-  //https://github.com/cujojs/when/issues/272
   var promise = when.unfold(unspool, predicate, handler, 0);
   return nodefn.bindCallback(promise, cb);
 }
 
 function identifyBS2(stream, options, cb) {
 
-    function version(){
-      var d = when.defer();
+  function version(){
+    return send(stream, 1000, options.version);
+  }
 
-      send(stream, 1000, options.version, function(err, result){
-        if(err){ return d.reject(err); }
+  var promise = challenge(stream, options)
+  .then(version)
+  .then(nodefn.lift(options.lookup));
 
-        return d.resolve(result);
-      });
-
-      return d.promise;
-    }
-
-    var promise = challenge(stream, options)
-    .then(version)
-    .then(nodefn.lift(options.lookup));
-
-    return nodefn.bindCallback(promise, cb);
+  return nodefn.bindCallback(promise, cb);
 }
 
 function bootload(stream, hex, cb){
 
-    //partially applying deprecated, so then how do you late bind?
-    //https://github.com/cujojs/when/issues/313
+  function upload(version){
+    return send(stream, 1000, hex)
+    .then(function(){
+      return version;
+    });
+  }
 
-    //and if I did then how would I get the version through then?
-    function upload(version){
-      var d = when.defer();
-
-      send(stream, 1000, hex, function(err, result){
-        if(err){ return d.reject(err); }
-
-        return d.resolve(version);
+  function signoff(version){
+    return when.promise(function(resolve, reject) {
+      stream.write(new Buffer([0]), function(err){
+        if(err){ return reject(err); }
+        return resolve(version);
       });
+    });
+  }
 
-      return d.promise;
-    }
+  var promise = identifyBS2(stream, revisions.bs2)
+  .then(upload)
+  .then(signoff);
 
-    function signoff(version){
-      var d = when.defer();
-
-      stream.write(new Buffer([0]), function(err, result){
-        if(err){ return d.reject(err); }
-
-        return d.resolve(version);
-      });
-
-      return d.promise;
-    }
-
-    var promise = identifyBS2(stream, revisions.bs2)
-    .then(upload)
-    .then(signoff);
-
-    return nodefn.bindCallback(promise, cb);
+  return nodefn.bindCallback(promise, cb);
 }
 
 module.exports = {
