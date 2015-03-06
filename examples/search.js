106a04b00167d9ccb23ef79374321ce37dc368ec
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-03-05 21:17:26 -0700

    cleanup search example

--- examples/search.js
+++ examples/search.js
@@ -1,117 +1,83 @@
 'use strict';
 
 var com = require('serialport');
-
 var when = require('when');
+var nodefn = require('when/node');
 
 var bs2 = require('../');
 
-var callbacks = require('when/callbacks');
-var fn   = require('when/function');
-var nodefn = require('when/node');
-
-//id like to lift all of serialport but that seems to break the constructor
-// var com = nodefn.liftAll(com);
-
-//replace with lifts when I figure out how that works
-
-// var id = when.lift(bs2.identifyBS2.bind(bs2));
-// var id = fn.lift(bs2.identifyBS2.bind(bs2));
-// var id = nodefn.lift(bs2.identifyBS2.bind(bs2));
-// var id = callbacks.lift(bs2.identifyBS2.bind(bs2));
-
-// var id = function(stream, options){
-//   var d = when.defer();
-
-//   bs2.identifyBS2(stream, options, function(err, board){
-//     if(err){ return d.reject(err); }
-
-//     return d.resolve(board);
-//   });
-
-//   return d.promise;
-// };
-
-function open(stream){
-  var d = when.defer();
-
-  stream.open(function(err){
-    if(err){ return d.reject(err); }
-
-    return d.resolve();
-  });
-
-  return d.promise;
-}
-
-function set(stream, options){
-  var d = when.defer();
-
-  stream.set(options, function(err){
-    if(err){ return d.reject(err); }
-
-    return d.resolve();
-  });
-
-  return d.promise;
-}
-
-function close(stream){
-  var d = when.defer();
-
-  stream.on('error', function(err){
-    return d.reject(err); 
-  });
-
-  stream.on('close', function(){
-    return d.resolve();
-  });
-
-  stream.close();
-  return d.promise;
-}
-
+// tries all revisions against the board
+// async series wont work here to my knownledge because its kind of
+// backwards, if error we need to continue to another board
+// if success stop and give result..
+// soo ... infinite promise chain instead? or bach?
 function search(path, done){
-
-  // async series wont work here to my knownledge because its kind of
-  // backwards, if error we need to continue to another board
-  // if success stop and give result..
-  // soo ... infinite promise chain instead? 
 
   var serialPort = new com.SerialPort(path, {
     baudrate: 9600,
   }, false);
 
-  // cant seem to lift the serialport either
-  // var sp = nodefn.liftAll(serialPort);
-
   var something = identify(serialPort, bs2.revisions.bs2);
-  nodefn.bindCallback(something, done);
+  return nodefn.bindCallback(something, done);
 
 }
  
 function identify(stream, rev){
-  return open(stream)
-   // return nodefn.call(stream.open.bind(stream))
-  .then(reset.bind(null, stream))
-  .then(bs2.identifyBS2.bind(null, stream, rev))
-  .finally(close.bind(null, stream));
+
+  function setDtr(){
+    return when.promise(function(resolve, reject) {
+      stream.set({dtr: false, brk: true}, function(err){
+        if(err){ return reject(err); }
+        return resolve();
+      });
+    });
+  }
+
+  function setBrk(){
+    return when.promise(function(resolve, reject) {
+      stream.set({dtr: true, brk: true}, function(err){
+        if(err){ return reject(err); }
+        return resolve();
+      });
+    });
+  }
+
+  function clear(){
+    return when.promise(function(resolve, reject) {
+      stream.set({dtr: true, brk: false}, function(err){
+        if(err){ return reject(err); }
+        return resolve();
+      });
+    });
+  }
+
+  function id(){
+    return bs2.identifyBS2(stream, rev);
+  }
+
+  function close(){
+  return when.promise(function(resolve, reject) {
+
+    stream.on('error', function(err){
+      return reject(err); 
+    });
+
+    stream.on('close', function(){
+      return resolve();
+    });
+
+    stream.close();
+  });
 }
 
-
-function reset(stream){
-
-
-  // var setBrk = nodefn.lift(stream.set);
-  // var clrBrk = nodefn.lift(stream.set)({brk: false});
-
-  // return nodefn.call(stream.set.bind(stream, {brk: true }))
-  // return setBrk({brk: false})
-  return set(stream, {brk: true})
+  return nodefn.lift(stream.open.bind(stream))()
+  .then(setDtr)
+  .delay(2)
+  .then(setBrk)
   .delay(45)
-  // .then(clrBrk);
-
-  .then(set.bind(null, stream, {brk: false}));
+  .then(clear)
+  .then(id)
+  .finally(close);
 }
 
 if(process && process.argv && process.argv[2])
