704b6b489ba2567111bb10036a227b44b52fa041
Blaine Bublitz <blaine@iceddev.com>
2015-03-30 19:01:35 -0700

    initial most impl

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var send = require('./lib/sendData');
 var revisions = require('./lib/revisions');
 
+var most = require('most');
 var when = require('when');
 var nodefn = require('when/node');
 
@@ -13,24 +14,25 @@ function challenge(stream, options, cb){
     return nodefn.bindCallback(when(0), cb);
   }
 
-  function unspool(index) {
-    return [index, index + 1];
-  }
-
   function predicate(index) {
-    return index >= options.challenge.length;
+    return index < options.challenge.length;
   }
 
   function handler(index) {
     return send(stream, 1000, options.challenge.slice(index, index + 1))
       .then(function(response){
         if(response !== options.response[index]){
-           throw new Error('Incorrect Response: ' + response + '. Board might not be a ' + options.name);
+          throw new Error('Incorrect Response: ' + response + '. Board might not be a ' + options.name);
+        } else {
+          return index + 1;
         }
       });
   }
 
-  var promise = when.unfold(unspool, predicate, handler, 0);
+  var promise = most.iterate(handler, 0)
+    .takeWhile(predicate)
+    .drain();
+
   return nodefn.bindCallback(promise, cb);
 }
 
@@ -62,25 +64,21 @@ function bootload(stream, type, hex, timeout, cb){
     throw new Error('Data must be in multiples of 18 bytes');
   }
 
-  function unspool(index) {
-    return index + pageSize;
-  }
-
-  function predicate(index) {
-    return index >= hex.length;
-  }
-
-  function handler(index) {
+  function iterator(index) {
     return send(stream, timeout, hex.slice(index, index + pageSize))
       .then(function(response){
         if(response){
           throw new Error('Board nacked packet ' + (index / pageSize) + ' with code: ' + response);
+        } else {
+          return index + pageSize;
         }
       });
   }
 
   function upload(){
-    return when.iterate(unspool, predicate, handler, 0);
+    return most.iterate(iterator, 0)
+      .take(hex.length / pageSize)
+      .drain();
   }
 
   function signoff(){
