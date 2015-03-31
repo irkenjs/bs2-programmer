21423c5e866d61c6f24464f5c1aab2f3d3eefb53
Blaine Bublitz <blaine@iceddev.com>
2015-03-30 20:15:53 -0700

    make programmer a class - still a lot of work/cleanup to do

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
 'use strict';
 
-var send = require('./lib/sendData');
 var revisions = require('./lib/revisions');
 
 var most = require('most');
 var when = require('when');
 var nodefn = require('when/node');
 
-var TimeoutError = require('./lib/timeouterror.js');
+function Programmer(options){
+  var self = this;
 
-function challenge(stream, options, cb){
-  if(!options.hasOwnProperty('challenge')){
+  var opts = options || {};
+
+  this._transport = opts.transport;
+
+  this._queue = null;
+
+  this._transport.on('data', function(chunk){
+    if(typeof self._queue === 'function'){
+      self._queue(chunk);
+    }
+  });
+}
+
+// Static method
+Programmer.revisions = revisions;
+
+Programmer.prototype.queueResponse = function(fn){
+  this._queue = fn;
+};
+
+Programmer.prototype.send = function(data, cb){
+  var self = this;
+
+  var responseLength = data.length + 1;
+
+  var promise = when.promise(function(resolve, reject) {
+
+    var buffer = new Buffer(0);
+    function onChunk(chunk) {
+      buffer = Buffer.concat([buffer, chunk]);
+      if (buffer.length > responseLength) {
+        // or ignore after
+        return reject(new Error('buffer overflow ' + buffer.length + ' > ' + responseLength));
+      }
+      if (buffer.length === responseLength) {
+        resolve(buffer[data.length]);
+      }
+    }
+
+    self.queueResponse(onChunk);
+
+    self._transport.write(data, function (writeError) {
+      if (writeError) {
+        return reject(writeError);
+      }
+    });
+  });
+
+  return nodefn.bindCallback(promise, cb);
+};
+
+Programmer.prototype.challenge = function(options, cb){
+  var self = this;
+
+  if(!options.challenge){
     return nodefn.bindCallback(when(0), cb);
   }
 
@@ -19,7 +72,8 @@ function challenge(stream, options, cb){
   }
 
   function handler(index) {
-    return send(stream, 1000, options.challenge.slice(index, index + 1))
+    return self.send(options.challenge.slice(index, index + 1))
+      .timeout(1000)
       .then(function(response){
         if(response !== options.response[index]){
           throw new Error('Incorrect Response: ' + response + '. Board might not be a ' + options.name);
@@ -34,38 +88,37 @@ function challenge(stream, options, cb){
     .drain();
 
   return nodefn.bindCallback(promise, cb);
-}
+};
 
-function identify(stream, options, cb) {
+Programmer.prototype.identify = function(options, cb){
+  var self = this;
 
-  function version(){
-    return send(stream, 1000, options.version);
-  }
-
-  var promise = challenge(stream, options)
-  .then(version)
-  .then(options.lookup)
-  // rewrite timeout errors
-  .catch(function(e){
-    if(e instanceof TimeoutError)
-    {
-      throw new Error(options.name + ' did not respond. Check power, connection, or maybe this is not a ' + options.name);
-    }
-    throw e;
-  });
+  var promise = this.challenge(options)
+    .then(function(){
+      return self.send(options.version);
+    })
+    .timeout(1000, new Error(options.name + ' did not respond. Check power, connection, or maybe this is not a ' + options.name))
+    .then(options.lookup);
 
   return nodefn.bindCallback(promise, cb);
-}
+};
 
-function bootload(stream, type, hex, timeout, cb){
+Programmer.prototype.bootload = function(options, hex, cb){
+  var self = this;
+
   var pageSize = 18;
 
   if(!hex || hex.length % pageSize !== 0){
     throw new Error('Data must be in multiples of 18 bytes');
   }
 
+  function predicate(index) {
+    return index < hex.length;
+  }
+
   function iterator(index) {
-    return send(stream, timeout, hex.slice(index, index + pageSize))
+    return self.send(hex.slice(index, index + pageSize))
+      .timeout(1000)
       .then(function(response){
         if(response){
           throw new Error('Board nacked packet ' + (index / pageSize) + ' with code: ' + response);
@@ -77,28 +130,24 @@ function bootload(stream, type, hex, timeout, cb){
 
   function upload(){
     return most.iterate(iterator, 0)
-      .take(hex.length / pageSize)
+      .takeWhile(predicate)
       .drain();
   }
 
   function signoff(){
     return when.promise(function(resolve, reject) {
-      stream.write(new Buffer([0]), function(err){
+      self._transport.write(new Buffer([0]), function(err){
         if(err){ return reject(err); }
         return resolve();
       });
     });
   }
 
-  var promise = identify(stream, type)
-  .then(upload)
-  .then(signoff);
+  var promise = self.identify(options)
+    .then(upload)
+    .then(signoff);
 
   return nodefn.bindCallback(promise, cb);
-}
-
-module.exports = {
-  identify: identify,
-  bootload: bootload,
-  revisions: revisions
 };
+
+module.exports = Programmer;
--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var Code = require('code');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 
-var bs2 = require('../');
+var Programmer = require('../');
 var hardware = require('../mock/hardware');
 
 var hi = new Buffer([0xFF, 0x00, 0x00, 0x00, 0x00, 0x30, 0xA0, 0xC7, 0x92, 0x66, 0x48, 0x13, 0x84, 0x4C, 0x35, 0x07, 0xC0, 0x4B]);
@@ -13,14 +13,17 @@ var blink = new Buffer([0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x
 lab.experiment('bs2', function () {
 
   var hw;
+  var bs2;
+
   lab.beforeEach(function (done) {
     hw = hardware();
+    bs2 = new Programmer({ transport: hw });
     done();
   });
 
   lab.test('times out', function (done) {
 
-    bs2.identify(hw, bs2.revisions.bs2, function(error){
+    bs2.identify(Programmer.revisions.bs2, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('BS2 did not respond. Check power, connection, or maybe this is not a BS2');
@@ -31,7 +34,7 @@ lab.experiment('bs2', function () {
   lab.test('fails on incorrect response', function (done) {
 
     hw.setData(new Buffer([0xC8, 0xAD, 0xCE, 0x10]));
-    bs2.identify(hw, bs2.revisions.bs2, function(error){
+    bs2.identify(Programmer.revisions.bs2, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Incorrect Response: 200. Board might not be a BS2');
@@ -42,7 +45,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2 1.0', function (done) {
 
     hw.setData(new Buffer([0xBE, 0xAD, 0xCE, 0x10]));
-    bs2.identify(hw, bs2.revisions.bs2, function(error, result){
+    bs2.identify(Programmer.revisions.bs2, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2', version: '1.0'});
@@ -53,7 +56,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2 less than one character', function (done) {
 
     hw.setData(new Buffer([0xBE, 0xAD, 0xCE, 0x00]));
-    bs2.identify(hw, bs2.revisions.bs2, function(error, result){
+    bs2.identify(Programmer.revisions.bs2, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2', version: '0'});
@@ -64,7 +67,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2e 1.0', function (done) {
 
     hw.setData(new Buffer([0x65]));
-    bs2.identify(hw, bs2.revisions.bs2e, function(error, result){
+    bs2.identify(Programmer.revisions.bs2e, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2e', version: '1.0'});
@@ -75,7 +78,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2e unknown', function (done) {
 
     hw.setData(new Buffer([0x02]));
-    bs2.identify(hw, bs2.revisions.bs2e, function(error){
+    bs2.identify(Programmer.revisions.bs2e, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Unknown: 2');
@@ -87,7 +90,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2sx 1.0', function (done) {
 
     hw.setData(new Buffer([0x58]));
-    bs2.identify(hw, bs2.revisions.bs2sx, function(error, result){
+    bs2.identify(Programmer.revisions.bs2sx, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2sx', version: '1.0'});
@@ -98,7 +101,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2sx 1.1', function (done) {
 
     hw.setData(new Buffer([0x59]));
-    bs2.identify(hw, bs2.revisions.bs2sx, function(error, result){
+    bs2.identify(Programmer.revisions.bs2sx, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2sx', version: '1.1'});
@@ -109,7 +112,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2sx 1.2', function (done) {
 
     hw.setData(new Buffer([0x60]));
-    bs2.identify(hw, bs2.revisions.bs2sx, function(error, result){
+    bs2.identify(Programmer.revisions.bs2sx, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2sx', version: '1.2'});
@@ -120,7 +123,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2sx unknown', function (done) {
 
     hw.setData(new Buffer([0x02]));
-    bs2.identify(hw, bs2.revisions.bs2sx, function(error){
+    bs2.identify(Programmer.revisions.bs2sx, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Unknown: 2');
@@ -131,7 +134,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.0', function (done) {
 
     hw.setData(new Buffer([0x70]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p24', version: '1.0'});
@@ -142,7 +145,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.1', function (done) {
 
     hw.setData(new Buffer([0x71]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p24', version: '1.1'});
@@ -153,7 +156,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.2', function (done) {
 
     hw.setData(new Buffer([0x72]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p24', version: '1.2'});
@@ -164,7 +167,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.3', function (done) {
 
     hw.setData(new Buffer([0x73]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p24', version: '1.3'});
@@ -175,7 +178,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.0', function (done) {
 
     hw.setData(new Buffer([0x50]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p40', version: '1.0'});
@@ -186,7 +189,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.1', function (done) {
 
     hw.setData(new Buffer([0x51]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p40', version: '1.1'});
@@ -197,7 +200,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.2', function (done) {
 
     hw.setData(new Buffer([0x52]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p40', version: '1.2'});
@@ -208,7 +211,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p 1.3', function (done) {
 
     hw.setData(new Buffer([0x53]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error, result){
+    bs2.identify(Programmer.revisions.bs2p, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2p40', version: '1.3'});
@@ -219,7 +222,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2p unknown', function (done) {
 
     hw.setData(new Buffer([0x02]));
-    bs2.identify(hw, bs2.revisions.bs2p, function(error){
+    bs2.identify(Programmer.revisions.bs2p, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Unknown: 2');
@@ -230,7 +233,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.0', function (done) {
 
     hw.setData(new Buffer([0x69]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe24', version: '1.0'});
@@ -241,7 +244,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.1', function (done) {
 
     hw.setData(new Buffer([0x70]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe24', version: '1.1'});
@@ -252,7 +255,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.2', function (done) {
 
     hw.setData(new Buffer([0x71]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe24', version: '1.2'});
@@ -263,7 +266,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.0', function (done) {
 
     hw.setData(new Buffer([0x49]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe40', version: '1.0'});
@@ -274,7 +277,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.1', function (done) {
 
     hw.setData(new Buffer([0x50]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe40', version: '1.1'});
@@ -285,7 +288,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe 1.2', function (done) {
 
     hw.setData(new Buffer([0x51]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error, result){
+    bs2.identify(Programmer.revisions.bs2pe, function(error, result){
 
       Code.expect(error).to.not.exist();
       Code.expect(result).to.deep.equal({name: 'BS2pe40', version: '1.2'});
@@ -296,7 +299,7 @@ lab.experiment('bs2', function () {
   lab.test('bs2pe unknown', function (done) {
 
     hw.setData(new Buffer([0x02]));
-    bs2.identify(hw, bs2.revisions.bs2pe, function(error){
+    bs2.identify(Programmer.revisions.bs2pe, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Unknown: 2');
@@ -307,7 +310,7 @@ lab.experiment('bs2', function () {
   lab.test('bootload throws with non multiple of 18 byte data', function (done) {
 
     var throws = function () {
-        bs2.bootload(hw, bs2.revisions.bs2, new Buffer([0x00, 0x01, 0x02, 0x03]), 1000, function(){});
+      bs2.bootload(Programmer.revisions.bs2, new Buffer([0x00, 0x01, 0x02, 0x03]), function(){});
     };
 
     Code.expect(throws).to.throw(Error, 'Data must be in multiples of 18 bytes');
@@ -318,7 +321,7 @@ lab.experiment('bs2', function () {
 
     //send bs2 response bytes, then the success byte
     hw.setData(new Buffer([0xBE, 0xAD, 0xCE, 0x10, 0x00]));
-    bs2.bootload(hw, bs2.revisions.bs2, hi, 1000, function(error){
+    bs2.bootload(Programmer.revisions.bs2, hi, function(error){
 
       Code.expect(error).to.not.exist();
       done();
@@ -329,7 +332,7 @@ lab.experiment('bs2', function () {
 
     //send bs2 response bytes, then 2 success bytes, 1 for each packet
     hw.setData(new Buffer([0xBE, 0xAD, 0xCE, 0x10, 0x00, 0x00]));
-    bs2.bootload(hw, bs2.revisions.bs2, blink, 1000, function(error){
+    bs2.bootload(Programmer.revisions.bs2, blink, function(error){
 
       Code.expect(error).to.not.exist();
       done();
@@ -340,7 +343,7 @@ lab.experiment('bs2', function () {
 
     //send bs2 response bytes, then the error byte which should stop bootload at first 18 byte packet
     hw.setData(new Buffer([0xBE, 0xAD, 0xCE, 0x10, 0x01]));
-    bs2.bootload(hw, bs2.revisions.bs2, blink, 1000, function(error){
+    bs2.bootload(Programmer.revisions.bs2, blink, function(error){
 
       Code.expect(error).to.exist();
       Code.expect(error.message).to.equal('Board nacked packet 0 with code: 1');
