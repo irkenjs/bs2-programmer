2678ca1890da1036adb8b7970f7f069b340b85cf
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-04-03 18:05:12 -0700

    challenge shouldnt be public, bring reset inside

--- examples/hi.js
+++ examples/hi.js
@@ -1,73 +1,50 @@
 /*eslint-disable no-process-exit */
 'use strict';
 
-var com = require('serialport');
-var when = require('when');
 var nodefn = require('when/node');
 
 var bs2 = require('../');
+var when = require('when');
+var Bs2Protocol = require('bs2-serialport');
+var SerialPort = require('serialport').SerialPort;
 
 var hex = new Buffer([0xFF, 0x00, 0x00, 0x00, 0x00, 0x30, 0xA0, 0xC7, 0x92, 0x66, 0x48, 0x13, 0x84, 0x4C, 0x35, 0x07, 0xC0, 0x4B]);
 
 function upload(path, done){
 
-  var serialPort = new com.SerialPort(path, {
-    baudrate: 9600
-  }, false);
+  var serialOptions = {
+    baudrate: 200
+  };
 
-  function setDtr(){
-    return when.promise(function(resolve, reject) {
-      serialPort.set({dtr: false, brk: true}, function(err){
-        if(err){ return reject(err); }
-        return resolve();
-      });
-    });
-  }
+  var serialport = new SerialPort(path, serialOptions);
 
-  function setBrk(){
-    return when.promise(function(resolve, reject) {
-      serialPort.set({dtr: true, brk: true}, function(err){
-        if(err){ return reject(err); }
-        return resolve();
-      });
-    });
-  }
+  var protocolOpts = {
+    serialport: serialport
+  };
 
-  function clear(){
-    return when.promise(function(resolve, reject) {
-      serialPort.set({dtr: true, brk: false}, function(err){
-        if(err){ return reject(err); }
-        return resolve();
-      });
-    });
-  }
+  var protocol = new Bs2Protocol(protocolOpts);
+
+  var bs2Options = {
+    protocol: protocol,
+    revision: 'bs2'
+  };
+
+  var open = nodefn.lift(serialport.open.bind(serialport));
 
   function bootload(){
-    return bs2.bootload(serialPort, bs2.revisions.bs2, hex, 1000);
+    return bs2.bootload(hex, bs2Options);
   }
 
   function close(){
-    return when.promise(function(resolve, reject) {
-
-      serialPort.on('error', function(err){
-        return reject(err);
-      });
-
-      serialPort.on('close', function(){
+    return when.promise(function(resolve) {
+      serialport.close(function(){
+        //always resolve so we dont overwrite previous errors in finally
         return resolve();
       });
-
-      serialPort.close();
     });
   }
 
-
-  var promise = nodefn.lift(serialPort.open.bind(serialPort))()
-  .then(setDtr)
-  .delay(2)
-  .then(setBrk)
-  .delay(45)
-  .then(clear)
+  var promise = open()
   .then(bootload)
   .finally(close);
 
@@ -79,7 +56,7 @@ if(process && process.argv && process.argv[2])
   upload(process.argv[2], function(error){
     if(error)
     {
-      console.log('error ', error);
+      console.log(error);
     }else{
       console.log('success');
     }
--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 'use strict';
+var nodefn = require('when/node');
 
 var Programmer = require('./lib/programmer');
 
 function identify(options, cb){
   var programmer = new Programmer(options);
 
-  return programmer.identify(cb);
-}
+  var version;
 
-function challenge(options, cb){
-  var programmer = new Programmer(options);
+  var promise = programmer.enterProgramming()
+    .then(programmer.identify.bind(programmer))
+    .then(function(e){
+      version = e;
+    })
+    .then(programmer.exitProgramming.bind(programmer))
+    .finally(function(e){
+      if(e instanceof 'Error'){
+        return e;
+      }
 
-  return programmer.challenge(cb);
+      return version;
+    });
+
+  return nodefn.bindCallback(promise, cb);
 }
 
 function bootload(hex, options, cb){
   var programmer = new Programmer(options);
 
-  return programmer.bootload(hex, cb);
+  var _bootload = function(){
+    return programmer.bootload(hex);
+  };
+
+  var promise = programmer.enterProgramming()
+    .then(_bootload)
+    .then(function(){
+      //hack because bootload is returning something which is being used as a callback in exit
+    })
+    .then(programmer.exitProgramming.bind(programmer));
+
+  return nodefn.bindCallback(promise, cb);
 }
 
 module.exports = {
   identify: identify,
-  challenge: challenge,
   bootload: bootload,
   revisions: Programmer.revisions,
   Programmer: Programmer
