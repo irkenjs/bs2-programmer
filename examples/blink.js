708a39ef6ec1437927fddd5c0c8e56bad73cf869
Blaine Bublitz <blaine@iceddev.com>
2015-04-08 19:12:00 -0700

    cleanup examples and make examples/search.js work

--- examples/blink.js
+++ examples/blink.js
@@ -2,24 +2,20 @@
 'use strict';
 
 var bs2 = require('../');
-var SerialPort = require('serialport').SerialPort;
-var Bs2SerialProtocol = require('bs2-serialport');
+var Bs2SerialProtocol = require('bs2-serial-protocol');
 
 var hex = new Buffer([0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x14, 0x20, 0x8c, 0x0e, 0xd8, 0xc8, 0x7c, 0xff, 0x0e, 0x60, 0x4a, 0xae, 0xe8, 0x9f, 0x49, 0xc1, 0x50, 0xc3, 0x6f, 0x8d, 0xd1, 0x03, 0x07, 0xc0, 0x60]);
 
 function bootload(path, done){
 
-  var serial = new SerialPort(path, { baudRate: 9600 }, false);
-  serial.open(function(){
-    var protocol = new Bs2SerialProtocol({ transport: serial });
+  var protocol = new Bs2SerialProtocol({ path: path });
 
-    var bs2Options = {
-      protocol: protocol,
-      revision: 'bs2'
-    };
+  var bs2Options = {
+    protocol: protocol,
+    revision: 'bs2'
+  };
 
-    return bs2.bootload(hex, bs2Options, done);
-  });
+  return bs2.bootload(hex, bs2Options, done);
 }
 
 if(process && process.argv && process.argv[2]){
--- examples/hi.js
+++ examples/hi.js
@@ -2,39 +2,32 @@
 'use strict';
 
 var bs2 = require('../');
-var Bs2Transport = require('bs2-serialport');
+var Bs2SerialProtocol = require('bs2-serial-protocol');
 
 var hex = new Buffer([0xFF, 0x00, 0x00, 0x00, 0x00, 0x30, 0xA0, 0xC7, 0x92, 0x66, 0x48, 0x13, 0x84, 0x4C, 0x35, 0x07, 0xC0, 0x4B]);
 
 function bootload(path, done){
 
-  var protocolOpts = {
-    path: path
-  };
-
-  var transport = new Bs2Transport(protocolOpts);
+  var protocol = new Bs2SerialProtocol({ path: path });
 
   var bs2Options = {
-    transport: transport,
+    protocol: protocol,
     revision: 'bs2'
   };
 
   return bs2.bootload(hex, bs2Options, done);
 }
 
-if(process && process.argv && process.argv[2])
-{
+if(process && process.argv && process.argv[2]){
   bootload(process.argv[2], function(error){
-    if(error)
-    {
+    if(error){
       console.log(error);
-    }else{
+    } else {
       console.log('success');
     }
     process.exit(0);
   });
-}else
-{
+} else {
   console.log('call with a path like /dev/tty.something');
   process.exit(0);
 }
